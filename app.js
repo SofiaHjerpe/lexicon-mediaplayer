29fc838497e54d32a20faf468752065107734365
Sofia Hjerpe <hjerpesofia@gmail.com>
2024-02-08 10:31:37 +0100

    fix play and pause function, add image on play

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { songs } from "./songs.js";
-
+let isPlaying = false;
 const audio = document.querySelector("audio");
 const audioPlayer = document.querySelector(".audio-player");
 const playingSong = document.querySelector(".playing");
@@ -32,6 +32,7 @@ audioPlayer.insertAdjacentHTML("beforeend", defaultTrackAsHtml);
 const playButtons = document.querySelectorAll(".icon-button .material-icons");
 
 const tracks = document.querySelectorAll(".visible-s-track");
+const trackImage = document.querySelector(".songImage");
 const previousSongs = document.querySelectorAll(".change-song-btn:nth-of-type(1)");
 const nextSongs = document.querySelectorAll(".change-song-btn:nth-of-type(2)");
 
@@ -39,75 +40,66 @@ playButtons.forEach((playButton) => {
   playButton.addEventListener("click", (e) => playMusic(e));
 });
 
-// function changeToPreviousSong(e, currentSong, songIndex, singleTracks, playButtons, tracks) {
+// function changeToPreviousSong(e, audio, songIndex,playButtons, tracks) {
 //   console.log(e.target.id);
 //   let index = e.target.id - 1;
 //   let preIndex = parseInt(index);
 //   let newSongIndex = songIndex - 1;
 //   if (newSongIndex != preIndex) {
-//     currentSong.setAttribute("src", arrayOfSongs[newSongIndex]);
-//     currentSong.pause();
-//     setPausedTrackStyling(songIndex, singleTracks, playButtons, tracks);
+//     audio.setAttribute("src", arrayOfSongs[newSongIndex]);
+//     audio.pause();
+//     setPausedTrackStyling(songIndex, , playButtons, tracks);
 //   } else if (newSongIndex === preIndex) {
-//     currentSong.setAttribute("src", arrayOfSongs[newSongIndex]);
-//     currentSong.play();
-//     setTrackStyling(songIndex, singleTracks, playButtons, tracks);
+//     audio.setAttribute("src", arrayOfSongs[newSongIndex]);
+//     audio.play();
+//     setTrackStyling(songIndex, , playButtons, tracks);
 //   }
 // }
 
- function onSongChange(e, currentSong, singleTracks, playButtons, tracks, songIndex) {
-   if (currentSong != e.target) {
-    currentSong.pause();
-    setPausedTrackStyling(songIndex, singleTracks, playButtons, tracks);
-   } else if (currentSong === e.target) {
-     currentSong.play();
-     setTrackStyling(songIndex, singleTracks, playButtons, tracks);
-   } else {
-     currentSong.pause();
-  }
-}
-// function styleWhenSongIsPaused(e, song) {
-// console.log("music is paused")
-// }
-// function setTrackStyling(songIndex, singleTracks, playButtons, tracks) {
-//   singleTracks.forEach((singleTrack) => {
-//     singleTrack.style.display = "none";
-//   });
-//   singleTracks[songIndex].style.display = "block";
-//   playButtons.forEach((playButton) => {
-//     playButton.innerText = "play_circle";
-//     playButton.classList.remove("highlightAudioIcon");
-//   });
-//   tracks.forEach((track) => {
-//     track.classList.remove("highlight-s-track");
-//   });
-// }
-
- function setPausedTrackStyling(songIndex, singleTracks, playButtons, tracks) {
-   singleTracks.forEach((singleTrack) => {
-     singleTrack.style.display = "none";
-   });
+function setTrackStyling(songIndex) {
   playButtons.forEach((playButton) => {
-     playButton.innerText = "play_circle";
+    playButton.innerText = "play_circle";
     playButton.classList.remove("highlightAudioIcon");
-   });
+  });
+  tracks.forEach((track) => {
+    track.classList.remove("highlight-s-track");
+  });
+  playButtons[songIndex].innerText = "pause_circle";
+  playButtons[songIndex].classList.add("highlightAudioIcon");
+  tracks[songIndex].classList.add("highlight-s-track");
+}
 
-   tracks.forEach((track) => {
-     track.classList.remove("highlight-s-track");
-   });
- }
+function setPausedTrackStyling(songIndex) {
+  playButtons.forEach((playButton) => {
+    playButton.innerText = "play_circle";
+    playButton.classList.remove("highlightAudioIcon");
+  });
+
+  tracks.forEach((track) => {
+    track.classList.remove("highlight-s-track");
+  });
+}
 
 function playMusic(e) {
   let index = e.target.id;
   let songIndex = parseInt(index);
 
+  trackImage.setAttribute("src", songs[songIndex].imgSrc);
   console.log(e.target.id);
-  let song = songs[songIndex].song;
-  if (audio.getAttribute("src" === song)) {
-    audio.pause();
-  } else {
-    audio.setAttribute("src", songs[songIndex].song);
-    audio.play();
-  }
 
+  audio.setAttribute("src", songs[songIndex].song);
+  audio.play();
+  isPlaying ? audio.pause() : audio.play();
+
+  audio.onplay = function () {
+    isPlaying = true;
+    setTrackStyling(songIndex, playButtons, tracks);
+  };
+
+  audio.onpause = function () {
+    isPlaying = false;
+    playButtons[songIndex].innerText = "play_circle";
+    //   btnv.innerHTML = "play_circle";
+    setPausedTrackStyling(songIndex);
+  };
 }
